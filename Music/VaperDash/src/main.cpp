e1c4c815f2449b8ad2275fbdd956005cc77f1530
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2025-05-07 01:55:24 -0400

    iOS + Reduce lag

--- Music/VaperDash/src/main.cpp
+++ Music/VaperDash/src/main.cpp
@@ -5,9 +5,17 @@
 
 using namespace geode::prelude;
 
+bool enabled = Mod::get()->getSettingValue<bool>("enabled");
+
+$execute {
+    listenForSettingChanges("enabled", [](bool value) {
+        enabled = value;
+    });
+}
+
 class $modify(FMODAudioEngine) {
     void loadMusic(gd::string path, float speed, float p2, float volume, bool shouldLoop, int musicID, int channelID, bool dontReset) {
-        if (Mod::get()->getSettingValue<bool>("enabled")) {
+        if (enabled) {
             FMODAudioEngine::loadMusic(path, 0.75, p2, volume, shouldLoop, musicID, channelID, dontReset);
         } else {
             FMODAudioEngine::loadMusic(path, speed, p2, volume, shouldLoop, musicID, channelID, dontReset);
@@ -17,7 +25,7 @@ class $modify(FMODAudioEngine) {
 
 class $modify(PlayLayer) {
     virtual void postUpdate(float p0) {
-        if (Mod::get()->getSettingValue<bool>("enabled")) {
+        if (enabled) {
             PlayLayer::applyTimeWarp(0.75);
             PlayLayer::postUpdate(p0);
             PlayLayer::m_isTestMode = true;
